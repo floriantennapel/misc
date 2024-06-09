7b88024b7a15ac4b19e0fe50a6acee7b75593c5b
Florian ten Napel <tennapel.f@gmail.com>
2024-06-09 22:50:50 +0200

    Update 2048.py

--- kattis/2048.py
+++ kattis/2048.py
@@ -1,51 +1,48 @@
 from sys import stdin
 
-merged = [[False for _ in range(4)] for _ in range(4)]
 
-def move(r, c, grid, dir):
+def move(r, c, grid, dir, merged):
     value = grid[r][c]
     params = [(0, -1), (len(grid) - 1, 1)]
     end, step = params[int(dir / 2)]
-    start = r if dir % 2 == 1 else c
+    start = c if dir % 2 == 0 else r
 
     for i in range(start, end, step):
-        next = i + step
-
         if dir % 2 == 0:
-            if grid[r][next] == value and not merged[r][next]:
-                grid[r][next] = 2*value
-                merged[r][next] = True
-                grid[r][i] = 0
-                return
-            elif grid[r][next] == 0:
-                grid[r][next] = value
-                grid[r][i] = 0
-            else:
-                return
+            row = next_row = r
+            col = i
+            next_col = i + step
         else:
-            if grid[next][c] == value and not merged[next][c]:
-                grid[next][c] = 2*value
-                merged[next][c] = True
-                grid[i][c] = 0
-                return
-            elif grid[next][c] == 0:
-                grid[next][c] = value
-                grid[i][c] = 0 
-            else:
-                return
+            row = i
+            next_row = i + step
+            col = next_col = c
 
-def move_all(grid, dir):
+        if grid[next_row][next_col] == value and not merged[next_row][next_col]:
+            grid[next_row][next_col] = 2*value
+            merged[next_row][next_col] = True
+            grid[row][col] = 0
+            return
+        elif grid[next_row][next_col] == 0:
+            grid[next_row][next_col] = value
+            grid[row][col] = 0
+        else:
+            return
+
+
+def move_all(grid, dir, merged):
     params = [(0, len(grid), 1), (len(grid)-1, -1, -1)]
     start, end, step = params[int(dir / 2)] 
     for i in range(start, end, step):
         for j in range(start, end, step):
-            move(i, j, grid, dir)
+            move(i, j, grid, dir, merged)
 
 
-lines = stdin.read().splitlines()
-grid = [[int(d) for d in line.split()] for line in lines[:-1]]
+if __name__ == '__main__':
+    lines = stdin.read().splitlines()
+    grid = [[int(d) for d in line.split()] for line in lines[:-1]]
+    merged = [[False for _ in range(4)] for _ in range(4)]
 
-move_all(grid, int(lines[4]))
+    move_all(grid, int(lines[4]), merged)
 
-for line in grid:
-    print(' '.join(map(str, line)))
+    for line in grid:
+        print(' '.join(map(str, line)))
